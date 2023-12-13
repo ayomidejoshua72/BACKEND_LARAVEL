db2abbaf3a156fa3ed1d74f5816b9c288feb9569
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-13 11:39:27 +0100

    route to the login page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
-    // dump("working");
     return view('welcome');
+});
+
+Route::get("login", function(){
+    return view("login");
 });
